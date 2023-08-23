4370e069ce9d3f9c026ecdf6540c2e47e4c773e1
kouki515 <71614240+kouki515@users.noreply.github.com>
2023-08-23 18:32:42 +0900

    modifies conf and deploy files

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/gettest', function() {
     return 'hello world';
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
